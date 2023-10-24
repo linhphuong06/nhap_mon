dca214907cfdc382105fe8e2adfd1a809b3d57ba
linhphuong06 <114252190+linhphuong06@users.noreply.github.com>
2023-10-24 14:25:27 +0700

    Add files via upload

---
+++ me_cung.py
@@ -0,0 +1,181 @@
+import heapq
+import numpy as np
+import random
+import time
+import psutil
+import resource 
+
+# Hàm tính khoảng cách Manhattan giữa hai điểm
+def manhattan_distance(point1, point2):
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
+# Hàm tìm đường đi bằng thuật toán A*
+def astar(maze, start, end):
+    open_list = []  # Danh sách mở
+    closed_list = []  # Danh sách đóng
+    print(len(maze))
+    # Mỗi nút trong danh sách mở được biểu diễn bằng (f, g, h, node)
+    start_node = (0, 0, manhattan_distance(start, end), start)
+    heapq.heappush(open_list, start_node)
+
+    # Lưu trữ cha của mỗi nút để tái tạo đường đi
+    came_from = {}
+
+    while open_list:
+        _, g, _, current_node = heapq.heappop(open_list)
+        for i in [(0, -1), (-1, 0), (1, 0), (0, 1)]:
+            if (start) == (end[0] + i[0], end[1] + i[1]):
+                return [start]
+        if current_node == end:
+            path = []
+            while current_node in came_from:
+                path.append(current_node)
+                current_node = came_from[current_node]
+            path.append(start)
+            path.reverse()
+            return path
+
+        closed_list.append(current_node)
+
+        for neighbor in [(0, -1), (-1, 0), (1, 0), (0, 1)]:
+            neighbor_node = (current_node[0] + neighbor[0], current_node[1] + neighbor[1])
+            # if neighbor_node[0] == -1 or neighbor_node[1] == -1 or neighbor_node[0] == rows or neighbor_node[1] == rows :
+            #     print("hang xom", neighbor_node)
+            # else:
+            #     print("hang xom", neighbor_node, maze[neighbor_node[0]][neighbor_node[1]])
+            # print("duong di", current_node, maze[current_node[0]][current_node[1]])
+            if (
+                0 <= neighbor_node[0] < len(maze) 
+                and 0 <= neighbor_node[1] < len(maze) 
+                and maze[neighbor_node[0]][neighbor_node[1]] == 0
+                and neighbor_node not in closed_list
+            ):
+                # print("nhan", neighbor_node)
+                # print("end", end, maze[end[0], end[1]])
+                tentative_g = g + 1
+                if neighbor_node not in [node for (_, _, _, node) in open_list]:
+                    heapq.heappush(open_list, (tentative_g + manhattan_distance(neighbor_node, end), tentative_g, manhattan_distance(neighbor_node, end), neighbor_node))
+                    came_from[neighbor_node] = current_node
+
+    return None  # Không tìm thấy đường đi
+def branch_and_bound(maze, start, end):
+    cols = len(maze)
+    open_list = [(0, start)]  # Hàng đợi ưu tiên với các bộ dữ liệu (chi phí, nút)
+    heapq.heapify(open_list)
+    closed_list = {}
+    came_from = {}
+
+    while open_list:
+        cost, current_node = heapq.heappop(open_list)
+        if current_node == end:
+            # Tạo lại đường dẫn từ điển 'cam_from'
+            path = []
+            while current_node in came_from:
+                path.append(current_node)
+                current_node = came_from[current_node]
+            path.append(start)
+            path.reverse()
+            return path  
+
+        closed_list[current_node] = cost
+
+        for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            row, col = current_node
+            new_row, new_col = row + dr, col + dc
+
+            if 0 <= new_row < cols and 0 <= new_col < cols and maze[new_row][new_col] == 0:
+                neighbor_node = (new_row, new_col)
+
+                new_cost = cost + 1  # chi phi la 1
+
+                if neighbor_node not in closed_list or new_cost < closed_list[neighbor_node]:
+                    heapq.heappush(open_list, (new_cost + (abs(neighbor_node[0] - end[0]) + abs(neighbor_node[1] + end[1])), neighbor_node))
+                    came_from[neighbor_node] = current_node
+
+    return None
+def dis(path, random_matrix, start, end):
+    if path:
+        count = 1
+        for row in range(len(random_matrix)):
+            for col in range(len(random_matrix[0])):
+                if (row, col) == start:
+                    print("S", end=" ")
+                elif (row, col) == end:
+                    print("E", end=" ")
+                elif (row, col) in path:
+                    print("*", end=" ")
+                    count += 1
+                elif random_matrix[row][col] == 0:
+                    print(".", end=" ")
+                else:
+                    print("#", end=" ")
+            print()
+        if (start == end): 
+            print("Số bước đi ngắn nhất: 0")
+        else:
+            print("Số bước đi ngắn nhất: ", count)
+    else:
+        for row in range(len(random_matrix)):
+            for col in range(len(random_matrix[0])):
+                if (row,col) == start:
+                    print("S", end = " ")
+                elif (row, col) == end:
+                    print("E", end=" ")
+                elif random_matrix[row, col] == 1:
+                    print("#", end = " ")
+                else:
+                    print(".", end= " ")
+            print() 
+        print("Không tìm thấy đường đi.")
+    
+def main():
+    request = input("Nhập yêu cầu (start/ end): ")
+    while (request == "start"):
+        # Bắt đầu đo thời gian
+        start_time = time.time()
+
+        # Tạo mê cung và tìm đường đi
+        num = random.randint(20, 50)
+        cols = random.randint(3, num)
+        random_matrix = np.random.randint(2, size=(cols, cols))
+        start = ((random.randint(0, cols-1)), (random.randint(0, cols-1)))
+        end = ((random.randint(0, cols-1)), (random.randint(0, cols-1)))
+        random_matrix[end[0]][end[1]] = 0
+        random_matrix[start[0]][start[1]] = 0
+
+        path = astar(random_matrix, start, end)
+        
+        # Dừng đo thời gian
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        # Đo bộ nhớ sử dụng
+        memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+        print("Mê cung:")
+        for row in range(len(random_matrix)):
+            for col in range(len(random_matrix[0])):
+                if (row,col) == start:
+                    print("S", end = " ")
+                elif (row, col) == end:
+                    print("E", end=" ")
+                else:
+                    print(random_matrix[row,col], end = " ")
+            print()
+
+        print("\nA*:")
+        dis(path, random_matrix, start, end)
+        print("Thời gian thực thi:", elapsed_time, "giây")
+        print("Bộ nhớ sử dụng:", memory_usage, "KB")
+
+        path = branch_and_bound(random_matrix, start, end)
+        print("\nNhanh cận:")
+        dis(path, random_matrix, start, end)
+        print("Thời gian thực thi:", elapsed_time, "giây")
+        print("Bộ nhớ sử dụng:", memory_usage, "KB")
+
+        print("------------------------------------------------------------")
+        request = input("Nhập yêu cầu (start/ end): ")
+
+if __name__ == "__main__":
+    main()
